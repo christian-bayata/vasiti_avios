6889ddd0f71721512d62f35af7627cd2b37a23cf
christian-bayata <franksagie1@gmail.com>
2022-03-08 09:24:06 +0100

    ClearDB cloud mysql database setup

--- config/config.js
+++ config/config.js
@@ -15,10 +15,10 @@ const envConfigs = {
     url: TEST_DATABASE_URL,
     dialect: 'mysql',
   },
-  // production: {
-  //   url: PROD_DATABASE_URL,
-  //   dialect: 'mysql',
-  // },
+  production: {
+    url: 'mysql://b4f3322cc5b93c:b0804dd8@us-cdbr-east-05.cleardb.net/heroku_29594656521cb42?reconnect=true',
+    dialect: 'mysql',
+  },
 };
 
 export default envConfigs;
--- settings.js
+++ settings.js
@@ -6,3 +6,4 @@ export const ENVIRONMENT = process.env.NODE_ENV || 'development';
 
 export const DEV_DATABASE_URL = process.env.DEV_DATABASE_URL;
 export const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
+export const PROD_DATABASE_URL = process.env.PROD_DATABASE_URL;
