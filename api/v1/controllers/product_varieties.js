91b498f0991690b19665508777f8bce237d8c15d
christian-bayata <franksagie1@gmail.com>
2022-03-08 08:39:56 +0100

    Completed the create and update functionalities for product varieties

--- api/v1/controllers/product_varieties.js
+++ api/v1/controllers/product_varieties.js
@@ -1,6 +1,6 @@
 import 'express-async-errors';
 import helper from '../../../helper/helper.js';
-import validateProduct from '../../../input-validation/product_varieties.js/validateProductVarieties.js';
+import validateProductVarieties from '../../../input-validation/product_varieties.js/validateProductVarieties.js';
 import ErrorHandler from '../../../utils/errorHandler.js';
 import status from 'http-status';
 import {
@@ -21,7 +21,7 @@ const productVarietiesController = {
    */
   async createProductVariants(req, res, next) {
     return helper(async () => {
-      const { error } = await validateProduct(req.body);
+      const { error } = await validateProductVarieties(req.body);
       if (error)
         return res.status(status.BAD_REQUEST).send(error.details[0].message);
       const { size, color, quantity, price } = req.body;
@@ -46,6 +46,37 @@ const productVarietiesController = {
       res.status(status.OK).send({
         success: true,
         productVariety,
+      });
+    }, next);
+  },
+  /**
+   * @Author - "Edomaruse, Frank"
+   * @Responsibilty - Updates product varieties
+   * @param req
+   * @param res
+   * @param next
+   * @route - /api/v1/update/product/variety/:id
+   * @returns {Object}
+   */
+  async updateProductVariant(req, res, next) {
+    return helper(async () => {
+      //Input validation with Joi
+      // const { error } = await validateProductVarieties(req.body);
+      // if (error)
+      //   return res.status(status.BAD_REQUEST).send(error.details[0].message);
+      let productVariety = await ProductVariety.findByPk(req.params.id);
+      if (!productVariety) {
+        return next(
+          new ErrorHandler(
+            'Product with the given id does not exist',
+            status.NOT_FOUND
+          )
+        );
+      }
+      await productVariety.update(req.body, { where: { id: req.params.id } });
+      res.status(status.OK).send({
+        success: true,
+        message: 'Product variety was successfully updated',
       });
     }, next);
   },
--- api/v1/routes/productVarieties.js
+++ api/v1/routes/productVarieties.js
@@ -5,5 +5,8 @@ import productVarietiesController from '../controllers/product_varieties.js';
 router
   .route('/product/variety')
   .post(productVarietiesController.createProductVariants);
+router
+  .route('/update/product/variety/:id')
+  .put(productVarietiesController.updateProductVariant);
 
 export default router;
